4b599b54414b0d4f0252c87907405bbe67f37f1e
David Bickford <drb56@pitt.edu>
2016-03-28 17:37:22 -0400

    hope I didn't do as badly as I think I did :(

--- template/quiz2.js
+++ template/quiz2.js
@@ -4,35 +4,46 @@
 	// Use ajax to reach the api endpoint
 	// Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
-	$mouseover = $('.mouseover');
-	$click     = $('.clikc');
-	$sub       = $('.submit');
+	$mouseover = $('.mouse-over');
+	$click     = $('.click');
+	$submit       = $('.submit');
+	$newbutton = $('.newbutton');
+	$this = $(this);
+	var data = $.getJSON("http://www.mattbowytz.com/simple_api.json",{data: "quizData"}, function( json ) {
+  				console.log( json);
+	});
 
-	$mouseover.on('mouseover', function() {
-		$this = $(this);
+	$mouseover.mouseover('mouse-over', function() {
 		$(this).html('Scrooge McDuck!');
 		$(this).height($(this).height() + 50);
 	});
 
 	$click.click('click', function() {
-		$this.hmtl('Peace Out!')
-		$(this).fadeout(1500);
+		$(this).html('Peace Out!');
+		$(this).fadeOut(1500);
 		return false;
 	});
 
-	$submit.on('submit', function(e) {
+	$submit.submit('submit', function(e) {
 		e.preventDefault();
 		if ($(this).find('input[type="text"]').val() !== '') {
 			$(this).find('input').foreach(function() {
-				$(this).fadeout('slow');
+				$(this).fadeOut('slow');
 			});
-			$(this).appendwith('<h2>Congratulations! You've entered some text!</h2>');
+			$(this).appendWith("<h2>Congratulations! You've entered some text!</h2>");
 		}
 	});
+	
+	$newbutton.click('click', function(){
+		for(var i=0; i<data.length; i++)
+			{
+				$(this).html(data[i]);
+			}		 
+	});
 
-	$(document).on(ready, function() {
-		setTimeout({
-			$timeout.fadeIn('slow');
+	$(".document").on('ready', function() {
+		setTimeout(function(){
+			$('#timeout').fadeIn('slow');
 		}, 1000);
 	});
 
